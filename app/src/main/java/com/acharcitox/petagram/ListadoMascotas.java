9923223cf30afd4ef6ca602ee28212d5c9a31c21
Marcelo <60859440+Acharcitox@users.noreply.github.com>
2020-10-15 17:38:51 -0300

    Tarea Semana 3 - Recycler View y Action View

--- app/src/main/java/com/acharcitox/petagram/ListadoMascotas.java
+++ app/src/main/java/com/acharcitox/petagram/ListadoMascotas.java
@@ -12,20 +12,19 @@ import android.widget.Toolbar;
 
 import java.util.ArrayList;
 
-
-public class MainActivity extends AppCompatActivity {
+public class ListadoMascotas extends AppCompatActivity {
 
     private ArrayList <Mascota> mascotas = new ArrayList<Mascota>();
     private RecyclerView listaMascotas;
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        listaMascotas = (RecyclerView) findViewById(R.id.rvMascotas);
 
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.listado_mascotas);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.actionBar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         mascotas.add(new Mascota(R.drawable.icon_perro, "Cucho", 15,false));
         mascotas.add(new Mascota(R.drawable.icon_perro1, "Micho", 16,true));
@@ -33,19 +32,18 @@ public class MainActivity extends AppCompatActivity {
         mascotas.add(new Mascota(R.drawable.icon_perro3, "Pepa", 17,true));
         mascotas.add(new Mascota(R.drawable.icon_perro4, "Flora", 20,false));
 
+        listaMascotas = (RecyclerView) findViewById(R.id.rv5Mascotas);
+
         LinearLayoutManager llm = new LinearLayoutManager(this);
         llm.setOrientation(LinearLayoutManager.VERTICAL);
         listaMascotas.setLayoutManager(llm);
         inicializarAdaptador();
 
-    }
 
+    }
     private void inicializarAdaptador(){
         MascotaAdaptador adaptador = new MascotaAdaptador(mascotas);
         listaMascotas.setAdapter(adaptador);
     }
-    public void ir5mascoras(){
-        Intent i = new Intent(this, ListadoMascotas.class);
-        startActivity(i);
-    }
+
 }
